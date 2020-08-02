761594ece54d6fe9dcd69c50c2d1edc6593367f1
Ricards Liskovskis <ricardsliskovskis@gmail.com>
2020-08-02 19:41:52 +0300

    Admin endpoint testing
    Implemented and tested API endpoint that gets all records and displays them to admin, test successful

---
+++ Izmainas/IzmainasAdmin/ViewModels/RecordsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using IzmainasAdmin.Models;
+using IzmainasAdmin.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzmainasAdmin.ViewModels
+{
+    public class RecordsViewModel : Screen
+    {
+        private readonly IRecordService _recordService;
+        private readonly IWindowManager _windowManager;
+
+        private BindingList<Record> _records;
+
+        public RecordsViewModel(IRecordService recordService, IWindowManager windowManager)
+        {
+            _recordService = recordService;
+            _windowManager = windowManager;
+        }
+
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            try
+            {
+                await LoadRecords();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private async Task LoadRecords()
+        {
+            var records = await _recordService.GetAll();
+            Records = new BindingList<Record>(records);
+        }
+
+        public BindingList<Record> Records
+        {
+            get { return _records; }
+            set
+            {
+                _records = value;
+                NotifyOfPropertyChange(() => Records);
+            }
+        }
+
+    }
+}
--- Izmainas/IzmainasAdmin/ViewModels/ShellViewModel.cs
+++ Izmainas/IzmainasAdmin/ViewModels/ShellViewModel.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace IzmainasAdmin.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        public ShellViewModel()
+        {
+            ActivateItemAsync(IoC.Get<RecordsViewModel>(), new CancellationToken());
+        }
+
+        public async Task ViewAll()
+        {
+            await ActivateItemAsync(IoC.Get<RecordsViewModel>(), new CancellationToken());
+        }
     }
 }
